cc958c2379f799a15ce96e28fefa9ef7e0acf2ec
Tcherno So <tcherno.so@hotmail.com>
2023-10-12 22:46:59 +0100

    Main Page Done

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Navbar from './navBar/navaBar'
+import Main from './Main/Main'
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col bg-black container mx-auto px-80 py-2'>
-      <Navbar />
-      </main>
+    <main className='flex min-h-screen flex-col bg-black'>
+      <div className="container mx-auto px-80 py-2">
+        <Main />
+      </div>
+    </main>
   )
 }
